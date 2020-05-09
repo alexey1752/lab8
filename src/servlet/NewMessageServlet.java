636e1a0ea7b2e4e8d47e69420653e71281fb7465
alexey1752 <alexey.borisovets@mail.ru>
2020-05-09 14:34:03 +0300

    update last interaction time

--- src/servlet/NewMessageServlet.java
+++ src/servlet/NewMessageServlet.java
@@ -9,26 +9,19 @@ import entity.*;
 
 public class NewMessageServlet extends ChatServlet {
     private static final long serialVersionUID = 1L;
-    protected void doPost(HttpServletRequest request, HttpServletResponse
-            response) throws ServletException, IOException {
-// По умолчанию используется кодировка ISO-8859. Так как мы
-// передаѐм данные в кодировке UTF-8
-// то необходимо установить соответствующую кодировку HTTP-запроса
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
         request.setCharacterEncoding("UTF-8");
-// Извлечь из HTTP-запроса параметр 'message'
         String message = (String)request.getParameter("message");
-// Если сообщение не пустое, то
         if (message!=null && !"".equals(message)) {
-// По имени из сессии получить ссылку на объект ChatUser
-            ChatUser author = activeUsers.get((String)
-                    request.getSession().getAttribute("name"));
+            ChatUser author = activeUsers.get((String) request.getSession().getAttribute("name"));
             synchronized (messages) {
-// Добавить в список сообщений новое
                 messages.add(new ChatMessage(message, author,
                         Calendar.getInstance().getTimeInMillis()));
             }
+            author.setLastInteractionTime(Calendar.getInstance().getTimeInMillis());
         }
-// Перенаправить пользователя на страницу с формой сообщения
         response.sendRedirect("/resource/compose_message.htm");
     }
 }
